d0f72b4645f7eba2106f1800de52daa4aaeea5eb
Sanjula thilan <99550726+demonthila@users.noreply.github.com>
2023-07-13 23:53:37 +0530

    my first application
    21928 hs thilan

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: HomePage(),
+    );
+  }
+}
